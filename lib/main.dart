43a9ef0e34e595cfa05dd001fe91865ffeff99e1
hunter17960 <am179604@gmail.com>
2023-03-07 06:39:44 +0200

    second app

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,13 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
-void main() => runApp(const MaterialApp(
-      home: MainApp(),
-    ));
-
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  List<Quote> quotes = [
-    Quote(text: 'first quote from me to you so be greatfull okay',author: 'ahmed'),
-    Quote(text: 'second quote from me to you so be greatfull okay',author: 'ahmed'),
-    Quote(text: 'third quote from me to you so be greatfull okay',author: 'ahmed'),
-    ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[900],
-        title: const Text(
-          'helooooo',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-          children: quotes.map((quote) => QuoteCard(
-            quote:quote,
-            delete:(){
-              setState(() {
-                quotes.remove(quote);
-              });
-            }
-            )).toList(),
-                  
-                  ),
-    );
-  }
-}
+import 'package:project__q/pages/choose_location.dart';
+import 'package:project__q/pages/home.dart';
+import 'package:project__q/pages/loading.dart';
+void main()=>runApp( MaterialApp(
+  initialRoute: '/home',
+ 
+  routes: {
+    '/':(context) => const Loading(),
+    '/home':(context) => const Home(),
+    '/location':(context) => const ChooseLocation()
+  },
+));
